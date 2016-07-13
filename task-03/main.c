f1c29baa344b1903f8b5cf9244864ed17749de3c
Cenk Gündoğan <cnkgndgn@gmail.com>
2016-07-13 19:51:59 +0200

    task-03: initial commit

---
+++ task-03/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "shell.h"
+#include "thread.h"
+
+char stack[THREAD_STACKSIZE_MAIN];
+
+void *thread_handler(void *arg)
+{
+    /* ... */
+    return NULL;
+}
+
+int main(void)
+{
+    puts("This is Task-03");
+
+    kernel_pid_t pid = thread_create(stack, sizeof(stack),
+                                     THREAD_PRIORITY_MAIN - 1,
+                                     THREAD_CREATE_STACKTEST,
+                                     thread_handler, NULL,
+                                     "thread");
+
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
