0158b91597df17fd2fb5199961bce5f5d08c69d8
Cenk Gündoğan <cnkgndgn@gmail.com>
2016-07-13 19:57:06 +0200

    task-04: minor changes

--- task-04/main.c
+++ task-04/main.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "shell.h"
+#include "thread.h"
 #include "xtimer.h"
+
+char stack[THREAD_STACKSIZE_MAIN];
+
+void *thread_handler(void *arg)
+{
+    /* ... */
+    return NULL;
+}
 
 int main(void)
 {
     puts("This is Task-04");
 
-    while(1) {
-        /* ... */
-        printf("%u\n", /* ... */);
-    }
+    kernel_pid_t pid = thread_create(stack, sizeof(stack),
+                                     THREAD_PRIORITY_MAIN - 1,
+                                     THREAD_CREATE_STACKTEST,
+                                     thread_handler, NULL,
+                                     "thread");
+
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
     return 0;
 }
