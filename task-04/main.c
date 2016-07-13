696cc3ad63fac9dd50d91efb3216091c309c1e32
Cenk Gündoğan <cnkgndgn@gmail.com>
2016-07-13 19:40:24 +0200

    task-04: initial commit

---
+++ task-04/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "xtimer.h"
+
+int main(void)
+{
+    puts("This is Task-04");
+
+    while(1) {
+        /* ... */
+        printf("%u\n", /* ... */);
+    }
+
+    return 0;
+}
